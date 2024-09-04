619fbbd86fb3cb7c6be7f2d852d63abb6782c1a3
Jamie McDade <jmcdade11@gmail.com>
2024-09-04 17:49:03 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
